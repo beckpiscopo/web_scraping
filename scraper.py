2c426c64f9c67ac6d6bf7b16a995ff1a68d3fe7d
brittbec <hello@brittbec.com>
2018-10-25 08:02:59 -0500

    Learned how to scrape links from a url

--- scraper.py
+++ scraper.py
@@ -1,7 +1,7 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-html = urlopen('https://treehouse-projects.github.io/horse-land/index.html')
+html = urlopen('http://www.ontheissues.org/states/AR.htm')
 soup = BeautifulSoup(html.read(), 'html.parser')
 
 # div = soup.find('div', {'class': 'featured'})
@@ -10,5 +10,8 @@ soup = BeautifulSoup(html.read(), 'html.parser')
 # featured_header = soup.find('div', {'class': 'featured'})
 # print(featured_header.get_text())
 
-for button in soup.find(attrs={'class': 'button button--primary'}): 
-    print(button)
+#for button in soup.find(attrs={'class': 'button button--primary'}): 
+#   print(button)
+
+for link in soup.find_all('a'): 
+    print(link.get('href'))
