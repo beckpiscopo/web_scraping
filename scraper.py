f99a351aa6e39b468b57b2a79ce7feaff0601444
brittbec <hello@brittbec.com>
2018-10-25 07:31:46 -0500

    learned about the find() method in python

--- scraper.py
+++ scraper.py
@@ -4,6 +4,8 @@ from bs4 import BeautifulSoup
 html = urlopen('https://treehouse-projects.github.io/horse-land/index.html')
 soup = BeautifulSoup(html.read(), 'html.parser')
 
-divs = soup.find_all('divs')
-for div in divs:
-    print(div)
+# div = soup.find('div', {'class': 'featured'})
+# print(div)
+
+featured_header = soup.find('div', {'class': 'featured'})
+print(featured_header.get_text())
