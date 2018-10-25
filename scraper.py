1c6fa65d27c4090346d34069957d39df7ae7daab
brittbec <hello@brittbec.com>
2018-10-25 07:35:53 -0500

    learned about the built-in feature of class: attrs={'class == class_

--- scraper.py
+++ scraper.py
@@ -7,5 +7,8 @@ soup = BeautifulSoup(html.read(), 'html.parser')
 # div = soup.find('div', {'class': 'featured'})
 # print(div)
 
-featured_header = soup.find('div', {'class': 'featured'})
-print(featured_header.get_text())
+# featured_header = soup.find('div', {'class': 'featured'})
+# print(featured_header.get_text())
+
+for button in soup.find(attrs={'class': 'button button--primary'}): 
+    print(button)
