95fb6ca000865094472451605f5db52cc498f930
brittbec <hello@brittbec.com>
2018-11-21 15:44:52 -0600

    fixed indentation issues in code

--- AraneaSpider/AraneaSpider/spiders/crawler.py
+++ AraneaSpider/AraneaSpider/spiders/crawler.py
@@ -2,17 +2,17 @@ from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 
 class HorseSpider(CrawlSpider): 
-name = 'Whirlaway'
-allowed_domains = ['treehouse-projects.github.io']
+    name = 'Whirlaway'
+    allowed_domains = ['treehouse-projects.github.io']
 
-start_urls = ['http://treehouse-projects.github.io/horse-land']
+    start_urls = ['http://treehouse-projects.github.io/horse-land']
 
-rules = [Rule(LinkExtractor(allow=r'.*'), 
-                callback='parse_horses', 
-                follow=True)]
+    rules = [Rule(LinkExtractor(allow=r'.*'), 
+                    callback='parse_horses', 
+                    follow=True)]
 
-def parse_horses(self, response): 
-    url = response.url
-    title = response.css('title')
-    print('Page URL: {}'.format(url))
-    print('Page title: {}'.format(title))
+    def parse_horses(self, response): 
+        url = response.url
+        title = response.css('title')
+        print('Page URL: {}'.format(url))
+        print('Page title: {}'.format(title))
